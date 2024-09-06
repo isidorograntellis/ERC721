f5d8851a9fa92659a6a74811172ab2dee6463637
isidorograntellis <isidorograntelliseb361r@hotmail.com>
2024-09-06 14:58:48 +0000

    Initialised

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
 contract TestNFT is ERC721, ERC721Burnable {
     constructor()
-        ERC721("cc","nn")
+        ERC721("gg","nn")
     {}
 
     function safeMint(address to, uint256 tokenId) public {
